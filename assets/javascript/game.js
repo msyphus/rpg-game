73bcb6dd65ece2f5f783e580c0a362ecd2c87045
msyphus <mark.syphus@gmail.com>
2020-03-16 23:57:36 -0600

    setup with id tags for js, updated character variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,37 @@
 var gato = {
+    name: "Gato",
     health: 120,
     attack: 10,
-    counter: 15
+    counter: 15,
+    quote: "Welcome to my jungle; I've got fun and games!",
+    attackQuote: "Benção!"
 }
 
 var rock = {
+    name: "Rock Norris",
     health: 130,
     attack: 15,
-    counter: 20
+    counter: 20,
+    quote: "Seriously?! Do I look like a guy you'd want to mess with?",
+    attackQuote: "My dad is Chuck!"
 }
 
 var crane = {
+    name: "Crane",
     health: 150,
     attack: 25,
-    counter: 25
+    counter: 25,
+    quote: "Forget the Karate Kid, I invented the crane kick!",
+    attackQuote: "I can beat you standing on one foot!"
 }
 
 var kama = {
+    name: "Kamaitachi",
     health: 100,
     attack: 6,
-    counter: 10
+    counter: 10,
+    quote: "Did you see that?...No?...My point exactly!",
+    attackQuote: "Ippon!"
 }
 
 //All character displayed in opponentLineUp
